f73f2d8ab230c65ddf393c96c61c5f67479e4a68
LicenceUHA <maarab@hotmail.fr>
2019-04-25 10:51:31 +0200

    deploy-prod reinstall

---
+++ deploy-prod.sh
@@ -0,0 +1,20 @@
+#recuperation des sources depuis gitHub
+git pull origin master
+
+# install des librairies
+composer install
+
+# mise a jour de la bdd drupal
+drush updb -y
+
+#Export des configs de prod
+drush csex prod -y
+
+#Ajout des config de prod
+git add config/prod
+git commit -m "ajout des config de prod"
+# import des configurations
+drush cim -y
+
+# vide des caches
+drush cr
